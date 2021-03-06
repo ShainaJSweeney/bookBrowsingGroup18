746193b71b3373f5a1225ae0cfc09a15ea2dc10a
Damian <dseym001@fiu.edu>
2021-03-05 23:14:52 -0500

    added cart child components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { WishComponent } from './wish/wish.component';
 import { ShopCartComponent } from './shop-cart/shop-cart.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { SaveForLaterComponent } from './save-for-later/save-for-later.component';
+import { CartItemsComponent } from './cart-items/cart-items.component';
+import { CartPriceComponent } from './cart-price/cart-price.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { FooterComponent } from './footer/footer.component';
     WishComponent,
     ShopCartComponent,
     TopBarComponent,
-    FooterComponent
+    FooterComponent,
+    SaveForLaterComponent,
+    CartItemsComponent,
+    CartPriceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cart-price/cart-price.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-price',
+  templateUrl: './cart-price.component.html',
+  styleUrls: ['./cart-price.component.css']
+})
+export class CartPriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
