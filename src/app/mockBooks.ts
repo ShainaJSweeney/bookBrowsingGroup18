b4909a6c12a095ecac90182cf47a246fac40d517
Damian <dseym001@fiu.edu>
2021-03-05 23:14:09 -0500

    added price attribute to hard coded data

--- src/app/mockBooks.ts
+++ src/app/mockBooks.ts
@@ -1,13 +1,13 @@
 import { Book } from './book';
 
 export const BOOKS: Book[] = [
-  { id: 11, name: 'The Little Prince' },
-  { id: 12, name: 'The Help' },
-  { id: 13, name: 'The Kite Runner' },
-  { id: 14, name: 'Gone Girl' },
-  { id: 15, name: 'Blind Spots' },
-  { id: 16, name: 'Where The Crawdads Sing' },
-  { id: 17, name: 'Diary Of A Wimpy Kid' },
-  { id: 18, name: 'The Lord Of The Rings' },
-  { id: 19, name: 'The Hobbit' }
+  { id: 11, name: 'The Little Prince', price: 10 },
+  { id: 12, name: 'The Help', price: 20 },
+  { id: 13, name: 'The Kite Runner', price: 30 },
+  { id: 14, name: 'Gone Girl', price: 40 },
+  { id: 15, name: 'Blind Spots', price: 50 },
+  { id: 16, name: 'Where The Crawdads Sing', price: 60 },
+  { id: 17, name: 'Diary Of A Wimpy Kid', price: 70 },
+  { id: 18, name: 'The Lord Of The Rings', price: 80 },
+  { id: 19, name: 'The Hobbit', price: 90 }
 ];
